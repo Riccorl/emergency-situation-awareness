0df50cdf0b92d3a8fc03998dba40b3eb3be2b76f
Francesco Grimaldi <csfrancescogrimaldi@gmail.com>
2019-06-20 16:54:42 +0200

    Remove REGEX

--- emergency-situation-awareness/config.py
+++ emergency-situation-awareness/config.py
@@ -1,6 +1,3 @@
 #Data
 CRISIS_NLP_VOLUNTEERS = '../resources/CrisisNLP_volunteers_labeled_data/CrisisNLP_volunteers_labeled_data'
 CRISIS_NLP_WORKERS = '../resources/CrisisNLP_labeled_data_crowdflower/CrisisNLP_labeled_data_crowdflower'
-
-#Preprocessing
-REGEX = "@\S+|https?:\S+|http?:\S|[^A-Za-z]+"
