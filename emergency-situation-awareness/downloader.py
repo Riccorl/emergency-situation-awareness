6ef1097fc1d6ff233da493e84e908a3d28abb32e
Riccardo Orlando <orlandoricc@gmail.com>
2019-06-27 15:53:24 +0200

    Fix tweet downloader.

--- emergency-situation-awareness/downloader.py
+++ emergency-situation-awareness/downloader.py
@@ -1,19 +1,17 @@
 import argparse
 import csv
-import logging
+from typing import List
 
-from tqdm import tqdm
-from twython import Twython
 import tweepy
 
 
-def tweet_dowloader(filename, path_outputh):
-    logging.basicConfig(
-        format="%(levelname)s - %(asctime)s: %(message)s",
-        datefmt="%H:%M:%S",
-        level=logging.INFO,
-    )
-
+def tweet_dowloader(path_input: str, path_outputh: str):
+    """
+    Download tweets from a list of ids.
+    :param path_input: ids in input.
+    :param path_outputh: where to save the tweets.
+    :return:
+    """
     consumer_key = "YOUR_KEY"
     consumer_secret = "YOUR_KEY"
     access_token = "YOUR_KEY"
@@ -23,57 +21,87 @@ def tweet_dowloader(filename, path_outputh):
     auth.set_access_token(access_token, access_token_secret)
 
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-    tweets_ids = _read_tweet_ids(filename)
+    tweets_ids = _read_tweet_ids(path_input)
     lookup_tweets(tweets_ids, api, path_outputh)
     return
 
 
-def lookup_tweets(tweet_ids, api, path_outputh):
+def lookup_tweets(tweet_ids: List, api: tweepy.API, path_outputh: str):
+    """
+    Retrieve tweets from a list of ids.
+    :param tweet_ids: tweet ids.
+    :param api: Tweepy API object.
+    :param path_outputh: where to save the tweets.
+    :return:
+    """
     tweet_count = len(tweet_ids)
     with open(path_outputh, mode="w", encoding="utf8") as out_file:
         try:
             for i in range((tweet_count // 100) + 1):
-                # Catch the last group if it is less than 100 tweets
                 end_loc = min((i + 1) * 100, tweet_count)
-                full_tweets = api.statuses_lookup(id_=tweet_ids[i * 100: end_loc], tweet_mode='extended')
-                # for j in range(i * 100, end_loc):
-                out_file.writelines(_fast_clean_tweet(t) + "\n" for t in full_tweets if t)
+                full_tweets = api.statuses_lookup(
+                    id_=tweet_ids[i * 100 : end_loc], tweet_mode="extended"
+                )
+                out_file.writelines(
+                    _fast_clean_tweet(t) + "\n" for t in full_tweets if t
+                )
         except tweepy.TweepError:
             print("Something went wrong, quitting...")
 
 
-def _read_tweet_ids(filename):
-    with open(filename, encoding="utf8") as csv_file:
+def _read_tweet_ids(path_input: str):
+    """
+    Read a file containing tweets id (crisis NLP tweets) and returns them as a list.
+    :param path_input: file containing ids.
+    :return: a list of tweet ids.
+    """
+    with open(path_input, encoding="utf8") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         next(csv_reader)
         return [row[0].replace("'", "") for row in csv_reader]
 
 
 def _fast_clean_tweet(tweet):
-    if tweet.retweeted_status:
-        print("it's retweet")
-        tweet_text = tweet.retweeted_status.full_text
-    else:
-        print("it's not retweet")
-        tweet_text = tweet.full_text
+    """
+    Remove new line char within text.
+    :param tweet: Tweet in input.
+    :return: text of the tweet.
+    """
+    tweet_text = _extract_text(tweet)
     tweet_text = tweet_text.strip()
-    tweet_text = tweet_text.replace('\n', '')
+    tweet_text = tweet_text.replace("\n", "")
+    return tweet_text
+
+
+def _extract_text(tweet):
+    """
+    Check if tweet is RT and returns the extended text of the tweet.
+    :param tweet: Tweet in input.
+    :return: non-cutted tweet text.
+    """
+    if hasattr(tweet, "retweeted_status"):
+        try:
+            tweet_text = tweet.retweeted_status.extended_tweet.full_text
+        except AttributeError:
+            tweet_text = tweet.retweeted_status.full_text
+    else:
+        try:
+            tweet_text = tweet.extended_tweet.full_text
+        except AttributeError:
+            tweet_text = tweet.full_text
     return tweet_text
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        help="input file",
-        dest="input",
-    )
-    parser.add_argument(
-        help="output file",
-        dest="output",
-    )
+    parser.add_argument(help="input file", dest="input")
+    parser.add_argument(help="output file", dest="output")
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # args = parse_args()
-    tweet_dowloader("../data/ids/2014-08-california_earthquake_2014_20140824_vol-1.json.csv", "../data/downloaded/et_california_2.txt")
+    tweet_dowloader(
+        "../data/ids/2014-08-california_earthquake_2014_20140824_vol-1.json.csv",
+        "../data/downloaded/et_california_2.txt",
+    )
